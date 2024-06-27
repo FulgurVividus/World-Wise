8e33bfcc9e5e99139b7a4e1a4b2386e01ca79860
FulgurVividus <murodjon.muzaffarov@gmail.com>
2024-06-27 21:43:17 +0500

    feat: new changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Products from "../pages/Products";
+import Pricing from "../pages/Pricing";
+import Homepage from "../pages/Homepage";
+import PageNotFound from "../pages/PageNotFound";
+import AppLayout from "../pages/AppLayout";
+import Login from "../pages/Login";
+
 function App() {
-  return <div>World Wise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="products" element={<Products />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
