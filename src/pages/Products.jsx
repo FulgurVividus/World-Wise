04948fb6bf41c846d43c216361a2a3de11514724
FulgurVividus <murodjon.muzaffarov@gmail.com>
2024-06-27 18:31:19 +0500

    feat: included navbar

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,13 @@
+import PageNav from "../components/PageNav";
+
 function Products() {
-  return <div>Products</div>;
+  return (
+    <div>
+      <PageNav />
+
+      <h1>Products</h1>
+    </div>
+  );
 }
 
 export default Products;
